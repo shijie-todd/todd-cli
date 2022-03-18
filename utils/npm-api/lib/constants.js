e2484783e222ba30a89b95e3e37940904e61e83f
shijie-todd <417138306@qq.com>
2022-03-18 17:00:28 +0800

    feat: utils/npm-api

---
+++ utils/npm-api/lib/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const DEFAULT_REGISTRY = 'https://registry.npmjs.org'
+const SUCCESS_CODE = 200
+
+module.exports = {
+	DEFAULT_REGISTRY,
+	SUCCESS_CODE,
+};
--- utils/npm-api/lib/index.js
+++ utils/npm-api/lib/index.js
@@ -1,7 +1,43 @@
 'use strict';
 
-module.exports = npmApi;
+const urlJoin = require('url-join');
+const axios = require('axios');
+const semver = require('semver');
 
-function npmApi() {
-    // TODO
+const { DEFAULT_REGISTRY, SUCCESS_CODE } = require('./constants')
+
+async function getNpmPkgInfo(pkgName, registry) {
+	const registryUrl = registry || DEFAULT_REGISTRY
+	const npmUrl = urlJoin(registryUrl, pkgName)
+	try {
+		const res = await axios.get(npmUrl)
+		if (res.status = SUCCESS_CODE) return res.data
+	} catch (e) {
+		throw new Error(e.message)
+	}
 }
+
+async function getNpmPkgVersions(pkgName, registry) {
+	const pkgInfo = await getNpmPkgInfo(pkgName, registry)
+	return Object.keys(pkgInfo.versions)
+}
+
+async function getAvailableHigherVersions(pkgName, baseVersion, registry) {
+	const allAvailableVersions = await getNpmPkgVersions(pkgName, registry)
+	return allAvailableVersions
+		.filter(version => semver.gt(version, baseVersion))
+		.sort((v1, v2) => { return semver.gt(v1, v2) ? -1 : 1 })
+}
+
+async function getPkgLatestVersion(pkgName, baseVersion, registry) {
+	const availableHigherVersions = await getAvailableHigherVersions(pkgName, baseVersion, registry);
+	return availableHigherVersions[0]
+}
+
+
+module.exports = {
+	// getNpmPkgInfo,
+	// getNpmPkgVersions,
+	// getAvailableHigherVersions,
+	getPkgLatestVersion
+};
